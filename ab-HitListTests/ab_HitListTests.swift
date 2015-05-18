dc03db009f7f9757aa30a8551f67bd64ef4d06d2
Aaron Bradley <abeats20@gmail.com>
2015-05-18 17:02:51 -0500

    Initial Commit

---
+++ ab-HitListTests/ab_HitListTests.swift
@@ -0,0 +1,36 @@
+//
+//  ab_HitListTests.swift
+//  ab-HitListTests
+//
+//  Created by Aaron Bradley on 5/18/15.
+//  Copyright (c) 2015 Aaron Bradley. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ab_HitListTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
